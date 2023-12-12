bae6b4d4a75e902010cf151ccbc7b8bc608d6c25
chp_h <veron.grigoreva@gmail.com>
2023-12-12 11:42:18 +0300

    Edits from teacher

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -23,6 +23,8 @@ void PrintMatrix(int[,] matrix)
         Console.WriteLine();
     }
 }
+
+/*Далее мое решение раотающее на процентов 20:
 int FindMinRow(int[,] matrix)
 {
     int Min = matrix[0, 0];
@@ -96,3 +98,45 @@ int MinCol = FindMinCol(matrix);
 Console.WriteLine("Массив после удаления строки и столбца с минимальным элементом:");
 InputNewMatrix(matrix, MinRow, MinCol);
 
+*/
+// Верное решение от преподавателя
+void SwapFirstLastString(int[,] matrix)
+{
+    int minValue = matrix[0, 0], minRow = 0, minColumn = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] <= minValue)
+            {
+                minValue = matrix[i, j];
+                minRow = i;
+                minColumn = j;
+            }
+        }
+    }
+    Console.WriteLine($"Минимум {minValue} на позиции({minRow + 1}, {minColumn + 1})");
+    Console.WriteLine();
+    Console.WriteLine("Конечный массив");
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        if (i != minRow)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (j != minColumn)
+                    Console.Write($"{matrix[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] coord = Console.ReadLine()!.Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[coord[0], coord[1]];
+Console.WriteLine("Начальный массив");
+InputMatrix(matrix);
+PrintMatrix(matrix);
+SwapFirstLastString(matrix);
